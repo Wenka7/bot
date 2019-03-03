7fc3f69d7de6ce8f92a7eb50da26bec70742ef83
Your Name <florentin.contreras@gmail.com>
2019-03-03 11:48:27 +0100

    init commit

---
+++ bot.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+function action(pattern,message,response) {
+    if(pattern === message.content)
+        message.reply(response)
+
+}
+
+bot.on('ready', function () {
+    console.log("Je suis connect� !")
+})
+
+
+bot.on('message', message => {
+    action("!cloud",message,"https://cloud.baptiste-contreras.fr")
+    action("salut",message,"ca va ?")
+})
+
+
+bot.login('')
